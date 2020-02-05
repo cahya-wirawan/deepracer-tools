36685cffa17f8a0880bc1b39bae8a199d39bbd59
Cahya Wirawan <cahya.wirawan@gmail.com>
2020-02-05 08:24:11 -0800

    added evdev and ROS codes

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -1,29 +1,34 @@
 from evdev import InputDevice, categorize, ecodes
 import rospy
 from std_msgs.msg import String
+from time import time
 
+ROS_RATE = 10
+TIME_DIFF = 1.0/ROS_RATE
 
-
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
 
 if __name__ == '__main__':
 
     dev = InputDevice('/dev/input/event1')
-
     print(dev)
+
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(ROS_RATE)  # 10hz
+    last_time = time() - TIME_DIFF
+
     for event in dev.read_loop():
+        now = time()
+        if now - last_time < TIME_DIFF:
+            continue
+        last_time = now
         if event.type == ecodes.EV_KEY:
             print(categorize(event))
-
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+            try:
+                if not rospy.is_shutdown():
+                    hello_str = "hello world %s" % rospy.get_time()
+                    rospy.loginfo(hello_str)
+                    pub.publish(hello_str)
+            except rospy.ROSInterruptException:
+                print("ROS exit")
+                exit(0)
