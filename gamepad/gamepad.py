a5b7e6621e2a834daf1636fd80792a980f05e01a
Cahya Wirawan <cahya.wirawan@gmail.com>
2020-02-06 03:53:19 -0800

    updated the gamed pad with the real ros message

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -2,11 +2,13 @@ from evdev import InputDevice, categorize, ecodes
 import evdev
 import rospy
 from std_msgs.msg import String
+from ctrl_pkg.msg import ServoCtrlMsg
 from time import time
 
 ROS_RATE = 10   # 10hz
 TIME_DIFF = 1.0/ROS_RATE
-SPEED_MAX = 0.7
+THROTTLE_MAX = 0.7
+
 
 def scale(val, src, dst):
     return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
@@ -22,29 +24,32 @@ if __name__ == '__main__':
     gamepad = evdev.InputDevice(devices[0].fn)
     print(gamepad)
 
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(ROS_RATE)
+    pub_manual_drive = rospy.Publisher('manual_drive', ServoCtrlMsg, queue_size=10)
+    msg = ServoCtrlMsg()
+    # rospy.init_node('talker', anonymous=True)
+    # rate = rospy.Rate(ROS_RATE)
     last_time = time() - TIME_DIFF
-    steering = 0
-    speed = 0
+    angle = 0
+    throttle = 0
 
     for event in gamepad.read_loop():
         # print(categorize(event))
         if event.type == 3:      # Analog stick
             if event.code == 0:  # X axis
-                steering = scale(event.value)
+                angle = scale(event.value)
             if event.code == 1:  # Y axis
-                speed = SPEED_MAX * scale(event.value)
+                throttle = THROTTLE_MAX * scale(event.value)
             now = time()
             if now - last_time < TIME_DIFF:
                 continue
             try:
                 if not rospy.is_shutdown():
-                    movement = "angle: %f, speed: %f".format(steering, speed)
-                    # pub.publish(movement)
-                    rospy.loginfo(movement)
-                    print(movement)
+                    # movement = "angle: %f, throttle: %f".format(angle, throttle)
+                    # print(movement)
+                    msg.angle = angle
+                    msg.throttle = throttle
+                    pub_manual_drive.publish(msg)
+                    rospy.loginfo(msg)
                     last_time = time()
             except rospy.ROSInterruptException:
                 print("ROS exit")
