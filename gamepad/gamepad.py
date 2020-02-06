0fcf4eb7fc86e8bdd29b8571f2c2c44514f6ba95
Cahya Wirawan <cahya.wirawan@gmail.com>
2020-02-06 03:17:24 -0800

    updated the gamed pad

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -1,34 +1,53 @@
 from evdev import InputDevice, categorize, ecodes
+import evdev
 import rospy
 from std_msgs.msg import String
 from time import time
 
 ROS_RATE = 10   # 10hz
 TIME_DIFF = 1.0/ROS_RATE
+SPEED_MAX = 0.7
+
+def scale(val, src, dst):
+    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
+
+
+def scale_stick(value):
+    return scale(value, (0, 255), (-1.0, 1.0))
 
 
 if __name__ == '__main__':
 
-    dev = InputDevice('/dev/input/event1')
-    print(dev)
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    gamepad = evdev.InputDevice(devices[0].fn)
+    print(gamepad)
 
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(ROS_RATE)
     last_time = time() - TIME_DIFF
+    steering = 0
+    speed = 0
 
-    for event in dev.read_loop():
-        now = time()
-        if now - last_time < TIME_DIFF:
-            continue
-        last_time = now
-        if event.type == ecodes.EV_KEY:
-            print(categorize(event))
+    for event in gamepad.read_loop():
+        # print(categorize(event))
+        if event.type == 3:      # Analog stick
+            if event.code == 0:  # X axis
+                steering = scale(event.value)
+            if event.code == 1:  # Y axis
+                speed = SPEED_MAX * scale(event.value)
+            now = time()
+            if now - last_time < TIME_DIFF:
+                continue
             try:
                 if not rospy.is_shutdown():
-                    hello_str = "hello world %s" % rospy.get_time()
-                    rospy.loginfo(hello_str)
-                    pub.publish(hello_str)
+                    movement = "angle: %f, speed: %f".format(steering, speed)
+                    # pub.publish(movement)
+                    rospy.loginfo(movement)
+                    print(movement)
+                    last_time = time()
             except rospy.ROSInterruptException:
                 print("ROS exit")
                 exit(0)
+
+
